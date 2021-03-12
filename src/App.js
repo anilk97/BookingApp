6e449c0a7dca22db223b5bfab637d1194ddcceff
Anil Kushwaha <anil.kushwaha@jellyfishtechnologies.com>
2021-03-12 23:34:51 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
+import Login from "./components/loginComponent";
+import SignUp from "./components/signupComponent";
 
 function App() {
     return (<Router>
